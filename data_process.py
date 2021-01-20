983102a99019a1068ef92c23463240736f3b0b03
ron159 <zlronron159@gmail.com>
2021-01-20 17:54:08 +0800

    修改一些细节&错误使其能够运行

---
+++ data_process.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from ipgeo import IpGeo
+
+def read_log_file(fname, out_csv):
+    str_flag = '2021'
+    print('reading raw text {}'.format(fname))
+    with open(fname, 'r') as fp:
+        text = fp.readlines()
+
+    content = [x.strip() for x in text]
+    filterd_lines = list(filter(lambda x: str_flag in x, content))
+    print('found {} connect to requests in total'.format(len(filterd_lines)))
+    filterd_lines = list(map(lambda x: x.split(' '), filterd_lines))
+
+    list_day = list(map(lambda x: x[0], filterd_lines))
+    list_time = list(map(lambda x: x[1], filterd_lines))
+    list_url = list(map(lambda x: x[4], filterd_lines))
+    list_domain = list(map(lambda x: x.split(':')[1], list_url))
+    list_port = list(map(lambda x: x.split(':')[2], list_url))
+
+    ipgeo = IpGeo('./geo/ip2region.db')
+
+    list_ipgeo = list(map(lambda x: ipgeo.find(x), list_domain))
+    list_ip4 = list(map(lambda x: x.get('ip'), list_ipgeo))
+    list_city_id = list(map(lambda x: x.get('city_id'), list_ipgeo))
+    list_country = list(map(lambda x: x.get('country'), list_ipgeo))
+    list_province = list(map(lambda x: x.get('province'), list_ipgeo))
+    list_city = list(map(lambda x: x.get('city'), list_ipgeo))
+    list_operator = list(map(lambda x: x.get('operator'), list_ipgeo))
+
+    df = pd.DataFrame({
+        'day': list_day,
+        'time': list_time,
+        'domain': list_domain,
+        'ip': list_ip4,
+        'port': list_port,
+        'city_id': list_city_id,
+        'country': list_country,
+        'province': list_province,
+        'city': list_city,
+        'operator': list_operator
+    })
+    df.to_csv(out_csv, index=False)
+    print('saved to {}'.format(out_csv))
+
+
+if __name__ == '__main__':
+    log_file = './log.txt'
+    out_csv = './logs.csv'
+    read_log_file(log_file, out_csv)
